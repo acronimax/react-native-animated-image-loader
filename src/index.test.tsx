5faa35ff6506557d9b0909ef1d2be9d101b0e591
acronimax <acronimax@gmail.com>
2024-07-18 19:08:05 -0600

    fix: test lib version

--- src/index.test.tsx
+++ src/index.test.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import { render } from '@testing-library/react-native';
+import { render, screen } from '@testing-library/react-native';
 import AnimatedImgLoader from './index';
 import { describe, expect, test } from '@jest/globals';
 
 describe('react-native-image-loader', () => {
   test('should render correctly', () => {
-    const wrapper = render(
+    render(
       <AnimatedImgLoader
         imageUri={
           'https://images.pexels.com/photos/1366919/pexels-photo-1366919.jpeg'
         }
       />
     );
-    expect(wrapper).toMatchSnapshot();
-    expect(wrapper).toBeDefined();
+    expect(screen).toMatchSnapshot();
+    expect(screen).toBeDefined();
   });
 });
